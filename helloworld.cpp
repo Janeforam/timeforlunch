439eb97cf0165853b6d82907d26552c05b2cc826
hereforam <hereforam@gmail.com>
2017-08-11 00:47:07 -0400

    strindex

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "strindex.h"
 
 int main(int argc, char* argv[]) {
-	printf("Hello World!\n");
+	char str1[10] = "abbcdef";
+	char str2[5] = "bc";
+	printf("starting index: %d\n", strindex(str1, str2));
 	return 0;
 }
---
+++ strindex.h
@@ -0,0 +1,4 @@
+#ifndef STRINDEX_H
+#define STRINDEX_H
+int strindex(char* s, char* t);
+#endif // !STRINDEX_H
